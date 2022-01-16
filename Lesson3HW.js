baf3b0b6eab2efe10e7f311a6bf31ea6b895755a
Alexey <alexsimbirov@gmail.com>
2022-01-16 23:56:51 +0200

    Added HW 3 Task 1 - Task 6

--- Lesson3HW.js
+++ Lesson3HW.js
@@ -1,30 +1,4 @@
-// function numberSum(a, b){
-//    return a + b
-// }
-// const result = numberSum(3, 5)
-// console.log(result)
-
-
-// const numArray = [1, 2, 3, 4, 5];
-// function sumArray(array){
-//    let sumNum = 0
-//    array.forEach(function(number){
-//       sumNum += number
-//    })
-//    return sumNum
-// }
-// const result2 = sumArray(numArray)
-// console.log(result2)
-
-// const numArray = [1, 2, 3, 4, 5];
-// // const result3 = numArray.reduce(function(acc, curr){
-// //    return acc += curr
-// // }, 0)
-// const result3 = numArray.reduce((acc, curr) => acc += Math.pow(curr, 2))
-// console.log(result3)
-
-
-//Task 1
+// Task 1
 const userNumber = +prompt('Enter any number');
 let squareNum = 0;
 for(let number = 1; number <= userNumber; number++){
@@ -32,12 +6,12 @@ for(let number = 1; number <= userNumber; number++){
 }
 console.log('Task 1 - ', squareNum)
 
-//Task 2
+// Task 2
 const  avg2 = (num1, num2) => (num1 + num2) / 2
 const resultTask2 = avg2(2, 5)
 console.log('Task 2 - ', resultTask2)
 
-//Task 3
+// Task 3
 const doubleArray = [[1, 6, 3, '6'], [10, 15, 13, '10']]
 const flatArray = doubleArray.flat(Infinity)
 const resultTask3 = flatArray.reduce((acc, curr) => {     
@@ -51,7 +25,7 @@ const resultTask3 = flatArray.reduce((acc, curr) => {
 }, 0)
 console.log('Task 3 - ', resultTask3)
 
-//Task 4
+// Task 4
 const newUser = {
    name: "Alex",
    age: 24
@@ -62,7 +36,7 @@ function addData(object){
 }
 addData(newUser)
 
-//Task 5
+// Task 5
 let flag = false;
 for(let i = 0; ; i++) {
    if(flag) {
@@ -71,4 +45,26 @@ for(let i = 0; ; i++) {
    flag = confirm()
 }
 
+
 // Task 6
+const someArray = [3, 7, -10, 20, '13', -1]
+
+// Сумма всех числовых типов данных
+const sumNumberArray = someArray.reduce((acc, curr) => {
+   return typeof curr === "number" ? acc += curr : acc
+}, 0)
+console.log('Task 6 - Сумма всех числовых типов данных - ', sumNumberArray)
+
+// Сортровка по убыванию
+someArray.sort((a, b) => (b - a));
+console.log('Task 6 - Сортровка по убыванию - ', someArray);
+
+// Массив с положительными числами
+const resultPositiveSort = someArray.filter(item => item > 0 && typeof item === 'number')
+console.log('Task 6 - Массив с положительными числами', resultPositiveSort)
+
+//Увелечение на 10, если тип данных - число
+const numberPlusTen = someArray.reduce((acc, curr) => {
+   return typeof curr === "number" ? acc += curr + 10 : acc
+}, 0)
+console.log('Task 6 - Увелечение на 10, если тип данных - число - ', numberPlusTen)
